3ba904f0577bb7e0496ff65777f1a63376242417
Anas Khan <anascoder4@gmail.com>
2024-10-14 19:22:25 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 
 const app = express()
-
+const PORT = process.env.PORT || 8000
 
 
 app.get("/"  , (req ,res)=>{
     res.json({message : "Hey from   AWS server Khush to bahot hoge tum !"})
 })
 
-app.listen(process.env.PORT ||  8000 , ()=>{
-    console.log(`Server listening `)
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`)
 })
