3ca949fd3644b301c734bafdb08f286040d2396b
Anas Khan <anascoder4@gmail.com>
2024-10-14 19:26:58 +0530

    updated

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/"  , (req ,res)=>{
     res.json({message : "Hey from   AWS server Khush to bahot hoge tum !"})
 })
 
+app.get("/api"  , (req,res)=>{
+    res.json({message  : "this is my api I have configure in it "})
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is listening on ${PORT}`)
 })
